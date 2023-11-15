225e8d2ed705fd427a0b6884735525969542d91f
Ujan RoyBandyopadhyay <ujan.roybandyopadhyay@drake.edu>
2023-11-15 17:20:37 -0600

    Add `API` interface

---
+++ apis/api.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from song import Song
+
+
+ID = str
+
+
+class API(ABC):
+    @abstractmethod
+    def search(self, song: Song) -> ID | None:
+        pass
+
+    @abstractmethod
+    def get_playlist(self, id: ID) -> list[Song]:
+        pass
+
+    @abstractmethod
+    def create_playlist(self, songs: list[Song]):
+        pass
