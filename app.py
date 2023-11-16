ee1cdfbe5f601225f85a074e2693200bd1faa172
Blythe Kelly <blythe.kelly@drake.edu>
2023-11-16 00:02:02 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -15,6 +15,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/library')
+def library():
+    return render_template('library.html')
+
+@app.route('/parse')
+def parse():
+    return render_template('parse.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__': 
     app.run()
