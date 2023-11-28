025cf6e023526d32fb0268fd9ea16c98b5563a2c
Ujan RoyBandyopadhyay <ujan.roybandyopadhyay@drake.edu>
2023-11-28 14:48:11 -0600

    Add JSON serialization methods to `Song`

--- apis/song.py
+++ apis/song.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import datetime
+import json
 
 
 @dataclass
@@ -8,3 +9,25 @@ class Song:
     artists: list[str]
     album: str | None = None
     released: datetime.date | None = None
+
+    def to_json(self) -> str:
+        return json.dumps({
+            'title': self.title,
+            'artists': self.artists,
+            'album': self.album,
+            'released': self.released.isoformat() if self.released else None
+        })
+
+    @classmethod
+    def from_json(cls, song: str) -> 'Song':
+        data = json.loads(song)
+
+        title = data['title']
+        artists = data['artists']
+        album = data['album']
+        if raw_date := data['released']:
+            released = datetime.datetime.fromisoformat(raw_date)
+        else:
+            released = None
+
+        return Song(title, artists, album, released)
