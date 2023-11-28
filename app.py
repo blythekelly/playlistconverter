2fbe3b1e6425d88dd5dba1310595205278698c6d
Ujan RoyBandyopadhyay <ujan.roybandyopadhyay@drake.edu>
2023-11-28 14:54:27 -0600

    Add support for multiple users

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from base64 import urlsafe_b64encode
 from hashlib import sha256
 import secrets
 from urllib.parse import quote, unquote, urlencode
+from uuid import uuid4
 
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, make_response, redirect, render_template, request
 
 from apis import API, SpotifyAPI
 from apis.song import Song
@@ -11,13 +12,13 @@ from apis.song import Song
 
 app = Flask(__name__)
 
-apis: dict[str, API | None] = {
-    'spotify': None,
-}
+services = ['spotify']
+
+apis: dict[str, dict[str, API | None]] = {}
 
 # temporary workaround to share state between functions for
 # authentication
-auth = {service: {} for service in apis}
+auth = {service: {} for service in services}
 
 
 def get_spotify_client_id() -> str:
@@ -50,11 +51,12 @@ def authenticate_spotify(dest: str):
         # TODO: Add 'state': <value>,
     })
 
-    auth['spotify']['verifier'] = verifier
-    auth['spotify']['redirect'] = redirect_uri
-
     URL = 'https://accounts.spotify.com/authorize'
-    return redirect(URL + '?' + params)
+    response = make_response(redirect(URL + '?' + params))
+    response.set_cookie('spotify-verifier', verifier.decode())
+    response.set_cookie('spotify-redirect', redirect_uri)
+
+    return response
 
 
 @app.route('/spotify-auth')
@@ -62,8 +64,12 @@ def complete_spotify_auth():
     if 'error' in request.args:
         return render_template('failed-authentication.html')
 
-    apis['spotify'] = SpotifyAPI(
-        request.args['code'], auth['spotify']['verifier'], auth['spotify']['redirect']
+    id = request.cookies['id']
+
+    apis[id]['spotify'] = SpotifyAPI(
+        request.args['code'],
+        request.cookies['spotify-verifier'],
+        request.cookies['spotify-redirect']
     )
 
     return redirect(unquote(request.args['dest']))
@@ -71,16 +77,24 @@ def complete_spotify_auth():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    template = render_template('index.html')
+    response = make_response(template)
+
+    id = str(uuid4())
+    apis[id] = {service: None for service in services}
+    response.set_cookie('id', id)
+    return response
 
 
 @app.route('/load-playlist')
 def load_playlist():
     service = request.args['service']
-    if service not in apis:
+    if service not in services:
         raise ValueError(f'unsupported service: {service}')
 
-    api = apis[service]
+    id = request.cookies['id']
+
+    api = apis[id][service]
     if api is None:
         return authenticate(service, redirect=request.full_path)
 
@@ -92,46 +106,55 @@ def load_playlist():
 @app.route('/convert-playlist')
 def convert_playlist():
     service = request.args['source']
-    if service not in apis:
+    if service not in services:
         raise ValueError(f'unsupported service: {service}')
 
-    api = apis[service]
+    id = request.cookies['id']
+    api = apis[id][service]
     if api is None:
         # TODO: Should we retry authentication?
         raise RuntimeError("couldn't access API")
 
-    session['songs'] = api.get_playlist(request.args['id'])
-    return render_template('convert-playlist.html')
+    template = render_template('convert-playlist.html')
+    response = make_response(template)
+    songs = '[' + ','.join(song.to_json() for song in api.get_playlist(request.args['id'])) + ']'
+    response.set_cookie('songs', songs)
+    return response
 
 
 @app.route('/save-playlist')
 def save_playlist():
     service = request.args['service']
-    if service not in apis:
+    if service not in service:
         raise ValueError(f'unsupported service: {service}')
 
-    api = apis[service]
+    id = request.cookies['id']
+    api = apis[id][service]
     if api is None:
         return authenticate(service, redirect=request.full_path)
 
-    session['service'] = service
-
-    return render_template('save-playlist.html')
+    template = render_template('save-playlist.html')
+    response = make_response(template)
+    response.set_cookie('service', service)
+    return response
 
 
 @app.route('/save-playlist', methods=['POST'])
 def save_playlist_post():
     name = request.form['name']
 
-    service = session['service']
-    if service not in apis:
+    service = request.cookies['service']
+    if service not in services:
         raise ValueError(f'unsupported service: {service}')
 
-    api = apis[service]
+    id = request.cookies['id']
+    api = apis[id][service]
     if api is None:
         raise RuntimeError("couldn't access API")
 
-    api.create_playlist(name, [Song(**song) for song in session['songs']])
+    songs = [Song.from_json(x) for x in request.cookies['songs']]
+    api.create_playlist(name, songs)
+
     return redirect('/')
 
 
